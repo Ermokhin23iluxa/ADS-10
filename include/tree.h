e1d1de5bf2d08e8872c954648f819671b16eb90e
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-06-05 00:00:39 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -5,53 +5,54 @@
 #include <vector>
 #include <algorithm>
 class Tree {
-    struct Node {
-        char c;
-        std::vector<Node*> tree_childs;
-    };
-    std::vector<char> perm;
-    Node* root = nullptr;
-    void create(std::vector<char> elements, Node* root) {
-        for (auto i : elements) {
-            if (i == root->c) {
-                elements.erase(std::remove(elements.begin(), elements.end(), i), elements.end());
-                for (auto ch : elements) {
-                    Node* temp = new Node;
-                    temp->c = ch;
-                    root->tree_childs.push_back(temp);
-                }
-            }
+  struct Node {
+    char c;
+    std::vector<Node*> tree_childs;
+  };
+  std::vector<char> perm;
+  Node* root = nullptr;
+  void create(std::vector<char> elements, Node* root) {
+    for (auto i : elements) {
+      if (i == root->c) {
+        elements.erase(std::remove(elements.begin(), elements.end(), i), elements.end());
+        for (auto ch : elements) {
+          Node* temp = new Node;
+          temp->c = ch;
+          root->tree_childs.push_back(temp);
         }
-        for (auto i : root->tree_childs) {
-            create(elements, i);
-        }
+      }
     }
-    void Perm(Node* root) {
-        if (root->tree_childs.empty()) {
-            perm.push_back(root->c);
-            permutations.push_back(perm);
-            perm.clear();
-        }
-        for (auto p : root->tree_childs) {
-            perm.push_back(root->c);
-            Perm(p);
-        }
+    for (auto i : root->tree_childs) {
+      create(elements, i);
     }
-public:
-    std::vector<std::vector<char>> permutations;
-    Tree(std::vector<char> elements) {
-        root = new Node;
-        root->c = ' ';
-        for (auto q : elements) {
-            Node* temp = new Node;
-            temp->c = q;
-            root->tree_childs.push_back(temp);
-        }
-        for (auto q : root->tree_childs) {
-            create(elements, q);
-            Perm(q);
-            perm.clear();
-        }
+  }
+  void Perm(Node* root) {
+    if (root->tree_childs.empty()) {
+      perm.push_back(root->c);
+      permutations.push_back(perm);
+      perm.clear();
     }
+    for (auto p : root->tree_childs) {
+      perm.push_back(root->c);
+      Perm(p);
+    }
+  }
+  
+ public:
+  std::vector<std::vector<char>> permutations;
+  Tree(std::vector<char> elements) {
+    root = new Node;
+    root->c = ' ';
+    for (auto q : elements) {
+      Node* temp = new Node;
+      temp->c = q;
+      root->tree_childs.push_back(temp);
+    }
+    for (auto q : root->tree_childs) {
+      create(elements, q);
+      Perm(q);
+      perm.clear();
+    }
+  }
 };
 #endif  // INCLUDE_TREE_H_
